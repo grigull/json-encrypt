5438067bc6b0e555a6d844f93516b4c759c2bdc8
grigull <robin.molengrigull@gmail.com>
2022-05-25 23:52:50 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # json-encrypt
+
+Script to encrypt/decrypt a JSON file
+
+```
+usage: ./json-encrypt [OPTIONS]
+
+  Input:
+    -e, --encrypt			Whether to encrypt or decrypt the input file
+    -k, --key <KEY>			Key to encrypt with
+    -i, --input <INPUT>	    Name of the decrypted/encrypted file
+    -o, --output <OUTPUT>	Name of the the encrypted/decrypted file
+```
---
+++ src/main.rs
@@ -0,0 +1,82 @@
+#[macro_use]
+extern crate magic_crypt;
+use clap::{self, Parser};
+use log;
+use magic_crypt::MagicCryptTrait;
+use serde_json;
+use std::{
+    error::Error,
+    ffi::OsStr,
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the the encrypted/decrypted file
+    #[clap(short, long)]
+    encrypt: bool,
+    /// Key to encrypt with
+    #[clap(short, long)]
+    key: String,
+    /// Name of the decrypted/encrypted file
+    #[clap(short, long)]
+    input: String,
+    /// Name of the decrypted/encrypted file
+    #[clap(short, long)]
+    output: String,
+}
+
+fn main() {
+    env_logger::init();
+    let args = Args::parse();
+
+    let result = || -> Result<(), Box<dyn Error>> {
+        if args.encrypt {
+            assert_eq!(
+                Path::new(&args.input)
+                    .extension()
+                    .and_then(OsStr::to_str)
+                    .unwrap(),
+                "json",
+                "Input file must end in .json"
+            );
+
+            let mc = new_magic_crypt!(args.key, 256);
+            let file = fs::read_to_string(args.input.clone())?;
+            let json: serde_json::Value = serde_json::from_str(&file)?;
+            let encrypted = mc.encrypt_str_to_base64(json.to_string());
+            let mut output = File::create(args.output.clone())?;
+            let _ = write!(output, "{}", &encrypted)?;
+            println!("JSON file: {} encrypted to {}", args.input, args.output);
+            Ok(())
+        } else {
+            assert_eq!(
+                Path::new(&args.output)
+                    .extension()
+                    .and_then(OsStr::to_str)
+                    .unwrap(),
+                "json",
+                "Output file must end in .json"
+            );
+
+            let mc = new_magic_crypt!(args.key, 256);
+            let file = fs::read_to_string(args.input.clone())?;
+            let decrypted = mc.decrypt_base64_to_string(&file)?;
+            let json_decrypted: serde_json::Value = serde_json::from_str(&decrypted)?;
+            let file = File::create(args.output.clone())?;
+            let _ = serde_json::to_writer(file, &json_decrypted)?;
+            println!(
+                "File: {} decrypted to JSON file {}",
+                args.input, args.output,
+            );
+            Ok(())
+        }
+    };
+
+    if let Err(e) = result() {
+        log::error!("{:?}", e);
+    };
+}
